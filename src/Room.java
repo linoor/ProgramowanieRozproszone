cc5ebb3c5b28f52688de302f93414c001d17afd9
= <michal.pomaranski@gmail.com>
2015-11-04 19:45:00 +0100

    simulating searching delay

--- src/Room.java
+++ src/Room.java
@@ -25,6 +25,11 @@ public class Room implements RoomInterface {
 
     @Override
     public boolean isExit() {
+        try {
+            Thread.sleep(500);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
         return isExit.get();
     }
 
