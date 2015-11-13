61bdbb53ac5730735931cbf6d70ac8740f846536
= <michal.pomaranski@gmail.com>
2015-11-13 16:15:17 +0100

    Task interface implemented

---
+++ src/Task.java
@@ -0,0 +1,45 @@
+/**
+ * Created by linoor on 11/13/15.
+ */
+public class Task implements  TaskInterface {
+
+    final private int firstQueue;
+    final private int lastQueue;
+
+    final private int taskId;
+
+    final private boolean keepOrder;
+
+    public Task(int firstQueue, int lastQueue, int taskId, boolean keepOrder) {
+        this.firstQueue = firstQueue;
+        this.lastQueue = lastQueue;
+        this.taskId = taskId;
+        this.keepOrder = keepOrder;
+    }
+
+    @Override
+    public int getFirstQueue() {
+        return firstQueue;
+    }
+
+    @Override
+    public int getLastQueue() {
+        return lastQueue;
+    }
+
+    @Override
+    public int getTaskID() {
+        return taskId;
+    }
+
+    @Override
+    public boolean keepOrder() {
+        return keepOrder;
+    }
+
+    @Override
+    public TaskInterface work(int queue) {
+        System.out.println(String.format("Task %d is working in queue %d...", getTaskID(), queue));
+        return this;
+    }
+}
