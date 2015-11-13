7efca6ba5282cd07393b05d6e5493122f0a85075
= <michal.pomaranski@gmail.com>
2015-11-13 14:47:33 +0100

    making clean master branch

--- src/Observer.java
+++
@@ -1,9 +0,0 @@
-/**
- * Created by linoor on 11/5/15.
- */
-public class Observer implements Runnable {
-    @Override
-    public void run() {
-        System.out.println("observer test");
-    }
-}
