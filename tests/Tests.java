877c5ff58d26ddf0af3ef5bebf76f32d6f7ef9b0
= <michal.pomaranski@gmail.com>
2015-11-13 17:23:17 +0100

    added simple test

---
+++ tests/Tests.java
@@ -0,0 +1,19 @@
+import org.junit.Test;
+
+/**
+ * Created by linoor on 11/13/15.
+ */
+public class Tests {
+
+    @Test
+    public void testQueueSystem() {
+        SystemInterface system = new System();
+        system.setNumberOfQueues(4);
+        system.setThreadsLimit(new int[] {
+          2, 3, 1, 2
+        });
+        system.addTask(new Task(0, 3 , 2, true));
+        system.addTask(new Task(0, 2 , 1, true));
+        system.addTask(new Task(1, 1 , 3, false));
+    }
+}
