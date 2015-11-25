9e6524431e26d35d42b5f3c15014d05d3d56fbba
= <michal.pomaranski@gmail.com>
2015-11-25 11:19:06 +0100

    name changing of the System class

--- tests/Tests.java
+++ tests/Tests.java
@@ -7,22 +7,22 @@ public class Tests {
 
     @Test
     public void testQueueSystem() {
-        System system = new System();
-        system.setNumberOfQueues(4);
-        system.setThreadsLimit(new int[] {
+        SystemExec systemExec = new SystemExec();
+        systemExec.setNumberOfQueues(4);
+        systemExec.setThreadsLimit(new int[] {
           1, 3, 1, 2
         });
-        system.addTask(new Task(0, 3 , 1, true));
-        system.addTask(new Task(0, 2 , 2, true));
-        system.addTask(new Task(2, 3, 3, false));
+        systemExec.addTask(new Task(0, 3 , 1, true));
+        systemExec.addTask(new Task(0, 2 , 2, true));
+        systemExec.addTask(new Task(2, 3, 3, false));
         try {
             Thread.sleep(2000);
-            system.addTask(new Task(1, 2 , 4, true));
+            systemExec.addTask(new Task(1, 2 , 4, true));
             Thread.sleep(500);
-            system.addTask(new Task(0, 3 , 5, false));
+            systemExec.addTask(new Task(0, 3 , 5, false));
         } catch (InterruptedException e) {
             e.printStackTrace();
         }
-        system.waitForFinish();
+        systemExec.waitForFinish();
     }
 }
