db820ca543b0fcd2f5c1643e519f09d7cff125cb
= <michal.pomaranski@gmail.com>
2015-12-08 19:29:01 +0100

    updated the script running tests

--- run_tests.sh
+++ run_tests.sh
@@ -3,6 +3,14 @@
 cp src_compiled/* oramus_tests/
 cd oramus_tests/
 javac PMO_StartTest.java
-time java PMO_StartTest
+
+# duplicate &1(stdout) to 5
+exec 5>&1
+output=$(time java PMO_StartTest | tee /dev/fd/5)
+
+echo -e "\n\n"
+echo -e "Errors:\n"
+echo "$output" | grep Blad
+
 source ~/.bashrc
 jobsdone;
