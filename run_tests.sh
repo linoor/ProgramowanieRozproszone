ebe73f7a069e5107672163e873c1c03269bdf358
= <michal.pomaranski@gmail.com>
2015-12-09 12:01:24 +0100

    added loop to test runner

--- run_tests.sh
+++ run_tests.sh
@@ -5,12 +5,15 @@ cd oramus_tests/
 javac PMO_StartTest.java
 
 # duplicate &1(stdout) to 5
-exec 5>&1
-output=$(time java PMO_StartTest | tee /dev/fd/5)
+for i in {1..1}
+do	
+	exec 5>&1
+	output=$(time java PMO_StartTest | tee /dev/fd/5)
 
-echo -e "\n\n"
-echo -e "Errors:\n"
-echo "$output" | grep Blad
+	echo -e "\n\n"
+	echo -e "Errors:\n"
+	echo "$output" | grep Blad
+done
 
 source ~/.bashrc
 jobsdone;
