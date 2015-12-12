c11e3a5aed2582b9f8b752505c659bd629de9cd7
= <michal.pomaranski@gmail.com>
2015-12-12 14:46:55 +0100

    generated server file

--- src_compiled/Start.java
+++ src_compiled/Start.java
@@ -1,5 +1,32 @@
+import org.omg.CORBA.IntHolder;
+
 /**
  * Created by linoor on 12/12/15.
  */
-public class Start {
+public class Start extends LinkExchangeSystemPOA {
+
+    @Override
+    public void register(String username, IntHolder userID) {
+
+    }
+
+    @Override
+    public void addLink(int userID, String link, IntHolder linkID) {
+
+    }
+
+    @Override
+    public boolean linkRemove(int userID, int linkID) {
+        return false;
+    }
+
+    @Override
+    public boolean publishLink(int userID, int linkID) {
+        return false;
+    }
+
+    @Override
+    public String[] getLinks(int userID) {
+        return new String[0];
+    }
 }
