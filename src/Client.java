3f0056556908382af28e2e26c052ee49b60b02c1
= <michal.pomaranski@gmail.com>
2015-12-12 15:20:19 +0100

    wrote basic client

---
+++ src/Client.java
@@ -0,0 +1,29 @@
+import org.omg.CORBA.ORB;
+import org.omg.CosNaming.NamingContextExt;
+import org.omg.CosNaming.NamingContextExtHelper;
+
+/**
+ * Created by linoor on 12/12/15.
+ */
+public class Client {
+
+    static LinkExchangeSystem exchangeSystem;
+
+    public static void main(String[] args) {
+        try {
+            // create and initialize the ORB
+            ORB orb = ORB.init(args, null);
+
+            // get the root naming context
+            org.omg.CORBA.Object objRef = orb.resolve_initial_references("LINKEXCHANGE");
+            NamingContextExt ncRef = NamingContextExtHelper.narrow(objRef);
+
+            String name = "LINKEXCHANGE";
+            exchangeSystem = LinkExchangeSystemHelper.narrow(ncRef.resolve_str(name));
+        } catch (Exception e) {
+            System.out.println("ERROR: " + e);
+            e.printStackTrace();
+        }
+    }
+
+}
