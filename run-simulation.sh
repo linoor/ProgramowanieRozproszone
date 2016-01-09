47cf483f2c0147a4d92c3cb2de0cdcc6587ec2d4
= <michal.pomaranski@gmail.com>
2016-01-09 10:22:50 +0100

    added more optimisation

--- run-simulation.sh
+++ run-simulation.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 # run the simulation
-mpicxx -O0 Helper.cpp Simulation.cpp main_01.cpp -o simulation || exit 1; # exit on failure
-mpirun -n $1 ./simulation
+mpicxx -O3 Helper.cpp Simulation.cpp main_01.cpp -o simulation || exit 1; # exit on failure
+mpirun -np $1 ./simulation
 
 source ~/.bashrc
 jobsdone
