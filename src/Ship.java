e65e5cae90cf431bf28d9a438c11dc246bd8148d
= <michal.pomaranski@gmail.com>
2016-01-10 21:28:36 +0100

    wrote the basic methods that will move the ships

--- src/Ship.java
+++ src/Ship.java
@@ -1,3 +1,5 @@
+import java.rmi.RemoteException;
+
 /**
  * Created by linoor on 1/10/16.
  */
@@ -11,5 +13,34 @@ public class Ship {
         this.playerId = playerId;
         this.gi = gi;
         this.warshipId = warshipId;
+    }
+
+    /**
+     *
+     * @return true if move successful
+     * @throws RemoteException
+     */
+    public boolean goUp() throws RemoteException {
+        GameInterface.PositionAndCourse positionAndCourse = gi.getMessage(playerId, warshipId);
+        if (positionAndCourse.getPosition().getCol() == gi.HIGHT-1) {
+            System.out.println(String.format("The ship %d can't go any further up", warshipId));
+            return false;
+        }
+
+        if (positionAndCourse.getCourse().)
+
+        return true;
+    }
+
+    public void goRight() {
+
+    }
+
+    public void goLeft() {
+
+    }
+
+    public void goDown() {
+
     }
 }
