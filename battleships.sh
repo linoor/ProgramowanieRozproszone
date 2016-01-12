56194720b50a3ec2088000f1e4c62944af8f234e
= <michal.pomaranski@gmail.com>
2016-01-12 19:40:09 +0100

    made the terminal screen in proper places on the screen

--- battleships.sh
+++ battleships.sh
@@ -4,11 +4,11 @@ cd src/
 # compile all of the files
 javac *.java
 # run the server
-xterm -T "Server" -e bash -c "java PMO_Server; bash" &
+gnome-terminal --geometry=80x16+21+6 -t "Server" -x bash -c "java PMO_Server; bash" &
 sleep 2
 # run the debugger
-xterm -T "debugger" -e bash -c "java DebugClient; bash" &
+gnome-terminal --geometry=80x28+629+4 -t "debugger" -x bash -c "java DebugClient; bash" &
 # run the first player
-xterm -T "do nothing player" -e bash -c "java DoNothingPlayer something; bash" &
+gnome-terminal --geometry=80x16+701-97 -t "do nothing player" -x bash -c "java DoNothingPlayer something; bash" &
 # run my player
-xterm -T "my player" -e bash -c "java Start myplayer; bash" &
+gnome-terminal --geometry=80x16+16+446 -t "my player" -x bash -c "java Start myplayer; bash" &
